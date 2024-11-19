01bd49b6a15bbc8944fbb5896145b6e51478b4c9
mdhabeebvulla <55741151+mdhabeebvulla@users.noreply.github.com>
2024-11-19 19:23:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ app = FastAPI()
 iris = load_iris()
 X = iris.data
 y = iris.target
+print(X)
 model = RandomForestClassifier(n_estimators=100)
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
 model.fit(X_train,y_train)
